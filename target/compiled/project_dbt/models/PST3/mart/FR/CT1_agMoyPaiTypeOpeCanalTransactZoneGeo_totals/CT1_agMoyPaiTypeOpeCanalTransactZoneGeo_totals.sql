f51a4e557dbc6d08e334529affa5ae04e7baaa7e
hkhnhan1994 <hkhnhan@gmail.com>
2025-07-31 09:58:05 +0700

    remove trash

--- target/compiled/project_dbt/models/PST3/mart/FR/CT1_agMoyPaiTypeOpeCanalTransactZoneGeo_totals/CT1_agMoyPaiTypeOpeCanalTransactZoneGeo_totals.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    payee_psp_country,
-    outbound_ibis_payments_trx_count,
-    outbound_ibis_payments_amount_sum_in_EUR,
-    SUM(outbound_ibis_payments_trx_count) OVER(PARTITION BY period) AS total_outbound_ibis_payments_trx_count,
-    SUM(outbound_ibis_payments_amount_sum_in_EUR) OVER(PARTITION BY period) AS total_outbound_ibis_payments_amount_sum_in_EUR,
-    load_timestamp,
-    period
-FROM `pj-bu-dw-data-sbx`.`dev_dm_pst3_FR`.`CT1_agMoyPaiTypeOpeCanalTransactZoneGeo`
--- target/compiled/project_dbt/models/STRH/warehouse/D_ENTERPRISE/dwh_strh_D_ENTERPRISE_CURRENT.sql
+++
@@ -1,8 +0,0 @@
-WITH current_table AS (
-    SELECT *,
-    ROW_NUMBER() OVER(PARTITION BY T_ROW_HASH ORDER BY T_LOAD_TIMESTAMP DESC) AS rn
-    FROM  `pj-bu-dw-data-sbx`.`dev_dwh_view_cmd`.`D_ENTERPRISE`
-    )
-SELECT * EXCEPT(rn)
-FROM current_table
-WHERE rn = 1
